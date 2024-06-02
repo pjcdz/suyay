7c1701d454615e78da1b800bbfcaae779000f84e
Pablo Cardozo <85005529+pjcdz@users.noreply.github.com>
2024-06-01 21:39:33 -0300

    asdasd

--- public/index.php
+++ public/index.php
@@ -81,13 +81,13 @@ $codHoras = ["codHoras0809", "codHoras0910", "codHoras1011", "codHoras1112", "co
     <div id="contenedor-principal">
         <div id="selector-consultorio">
         <select id="consultorioSelect" onchange="window.location.href=this.value;">
-                <option value="index/<?php echo $codConsultorio; ?>">Consultorio seleccionado: <?php echo $codConsultorio; ?></option>
-                <option value="index/1">Consultorio 1</option>
-                <option value="index/2">Consultorio 2</option>
-                <option value="index/3">Consultorio 3</option>
-                <option value="index/4">Consultorio 4</option>
-                <option value="index/5">Consultorio 5</option>
-                <option value="index/6">Consultorio 6</option>
+                <option value="/<?php echo $codConsultorio; ?>">Consultorio seleccionado: <?php echo $codConsultorio; ?></option>
+                <option value="/1">Consultorio 1</option>
+                <option value="/2">Consultorio 2</option>
+                <option value="/3">Consultorio 3</option>
+                <option value="/4">Consultorio 4</option>
+                <option value="/5">Consultorio 5</option>
+                <option value="/6">Consultorio 6</option>
             </select>
         </div>
         <div id="calendario">
